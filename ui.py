ef1e72072b8c85dc81af97d57c71f2b7a41f824e
wangzidong <wangzidong@meituan.com>
2024-07-12 16:02:26 +0800

    feat: 基本可用

--- ui.py
+++ ui.py
@@ -1,5 +1,7 @@
 import gradio as gr
 import json
+import re
+
 
 # pip install -r requirements.txt
 #
@@ -43,105 +45,118 @@ def load_config():
         drop_down_config = json.load(file)
 
 
-
-def get_hua_feng_list():
-    return [
-        "无",
-        "Pan Tianshou ----【古风画风1-成熟】",
-        "Zhu Qizhan ----【古风画风2-艳丽】",
-        "Qi Baishi ----【古风画风3-简约】",
-        "Zhang Daqian ----【古风画风4-可爱】",
-        "Henri Matisse ----【都市画风1-时尚】",
-        "Georgia O'Keeffe ----【都市画风2-欧美】",
-        "Craig Thompson ----【都市画风3-细腻】",
-        "Henri Matisse ----【都市画风4-流行】",
-        "Junji Ito - 代表作品为《die》等----【日式恐怖画风】",
-        "Park Tae-joon - 代表作品为《muming》等----【小鲜肉都市】",
-        "Park Ji-min - 代表作品为《kiming》等----【都市恋爱】",
-        "Kim Hyeong-seop - 代表作品为《CC LOVE》等----【韩风都市恋爱】",
-        "Kyoji Asano - 代表作品为《TK war》等----【细腻日漫】",
-        "Carlos Dattoli - 代表作品为《Iron Man》等----【都市少男风格】",
-        "Kuvshinov Ilya  - 代表作品为《CClove》等----【都市少女风格】",
-        "Gary Larson, Gerard Houckgeest - 代表作品为《R》等----【美式漫画】",
-        "Ruan Jia - 代表作品为《龙》等----【国风插画】",
-        "Paolo Roversi, Cecily Brown - 代表作品为《T》等----【偏质感】",
-        " Andreas Rocha  - 代表作品为《fun-class》等----【细节色彩】",
-        "Ryohka - 代表作品为《小林家的龙女仆》等",
-        "Rebecca Guay - 代表作品为《魔戒》等 ---【西方魔幻】",
-        "Craig Mullins - 代表作品为《Halo 4》等---【末日科幻】",
-        "Osamu Kobayashi - 代表作品为《Beck》等",
-        "Ryoichi Ikegami - 代表作品为《浪客剑心》等",
-        "Tite Kubo - 代表作品为《死神》等",
-        "Hajime Isayama - 代表作品为《进击的巨人》等",
-        "Makoto Shinkai - 代表作品为《你的名字。》等",
-        "Naoko Takeuchi - 代表作品为《美少女战士》等",
-        "Koyori - 代表作品为《只有我能进入的隐藏迷宫》等",
-        "Mamoru Hosoda (细田守) - 代表作品为《时之歌》、《未来的未来》等",
-        "Akira Toriyama - 代表作品为《龙珠》等",
-        "Rumiko Takahashi - 代表作品为《犬夜叉》、《美少女战士》等",
-        "Hideaki Anno - 代表作品为《新世纪福音战士》等", "Hayao Miyazaki - 代表作品为《龙猫》、《千与千寻》等---【经典动画】",
-        "Hiromasa Yonebayashi - 代表作品为《借物少女艾莉緹》",
-        "Kazuo Oga - 代表作品为《魔女宅急便》等", "Naoko Yamada - 代表作品为《声之形》、《春物》等",
-        "Takahiro Kimura - 代表作品为《Code Geass 反叛的鲁路修》等",
-        "Masashi Kishimoto - 代表作品为《火影忍者》等",
-        "Walt Disney - 代表作品为《白雪公主与七个小矮人》等",
-        "John Lasseter - 代表作品为《海底总动员》等---【皮克斯经典】",
-        "Tim Burton - 代表作品为《魔发奇缘》等",
-        "Glen Keane - 代表作品为《美女与野兽》等---【经典迪士尼】",
-        "Genndy Tartakovsky - 代表作品为《星球大战：克隆人战争》等",
-        "Chuck Jones - 代表作品为《汤姆猫和杰瑞鼠》等",
-        "Isao Takahata - 代表作品为《火垂るの墓》"
-    ]
+def parse_drop_down_desc(name, desc):
+    ret = get_x_list_item(name, desc)
+    return ret["tag"] if ret is not None else ""
 
 
-def get_out_mode_list():
-    return ["无", "黑白漫画模式", "彩色动画模式", "电影模式", "插画模式", "素描模式", "水彩模式", "游戏立绘模式",
-            "像素模式", "图标模式"]
+def parse_check_box_desc(name, select_list):
+    ret = ""
+    for list_item in select_list:
+        obj = get_x_list_item(name, list_item)
+        if obj is not None:
+            ret += obj["tag"] + ","
+    return ret
 
 
-def generater_tags(quality_text, hua_feng, out_mode, people_desc, movement_desc, scene_desc,
-                                    expression_desc, master_desc):
-    print(quality_text, hua_feng, out_mode, people_desc, movement_desc, scene_desc,
-                                    expression_desc, master_desc)
-    return '123'
+def replace_comma_space(string):
+    pattern = r',+(\s*,+)*'
+    string = re.sub(pattern, ',', string)
+    pattern = r'\s+,'
+    return re.sub(pattern, ',', string)
 
 
-def get_people_list():
-    return []
+def generater_tags(common_text_list, out_mode, people_desc, movement_desc, scene_desc,
+                   expression_desc, other_desc_list_select, r_desc_list_select,
+                   master_desc, clothes_desc, view_desc):
+    ret_list = []
+    # 基础描述
+    common_desc_text = parse_check_box_desc("commons", common_text_list)
+    # 输出模式
+    mode_desc_text = parse_drop_down_desc("modes", out_mode)
+    # 人物描述
+    people_desc_text = parse_drop_down_desc("masters", people_desc)
+    # 动作描述
+    pose_desc_text = parse_drop_down_desc("poses", movement_desc)
+    # 场景环境描述
+    scene_desc_text = parse_drop_down_desc("scenes", scene_desc)
+    # 表情情绪
+    expression_desc_text = parse_drop_down_desc("expressions", expression_desc)
+    # 其他描述
+    other_desc_text = parse_check_box_desc("others", other_desc_list_select)
+    # R级描述
+    R_DESC_text = parse_check_box_desc("R_DESC", r_desc_list_select)
+    # 自定义主体描述
+    master_desc_text = master_desc
+    # 服饰描述
+    clothes_desc_text = parse_drop_down_desc("clothes", clothes_desc)
+    # 观察者角度
+    view_desc_text = parse_drop_down_desc("views", view_desc)
+
+    print("common_desc_text:", common_desc_text)
+    print("mode_desc_text:", mode_desc_text)
+    print("people_desc_text:", people_desc_text)
+    print("pose_desc_text:", pose_desc_text)
+    print("scene_desc_text:", scene_desc_text)
+    print("expression_desc_text:", expression_desc_text)
+    print("other_desc_text:", other_desc_text)
+    print("R_DESC_text:", R_DESC_text)
+    print("master_desc_text:", master_desc_text)
+    print("clothes_desc_text:", clothes_desc_text)
+    print("view_desc_text:", view_desc_text)
+
+    # 排序 基础描述+自定义主体描述+人物描述+服饰描述+动作描述+R级描述+场景环境描述+表情情绪+其他描述+输出模式+观察者角度
+    ret_list.append(common_desc_text)
+    ret_list.append(master_desc_text)
+    ret_list.append(people_desc_text)
+    ret_list.append(clothes_desc_text)
+    ret_list.append(pose_desc_text)
+    ret_list.append(R_DESC_text)
+    ret_list.append(scene_desc_text)
+    ret_list.append(expression_desc_text)
+    ret_list.append(other_desc_text)
+    ret_list.append(mode_desc_text)
+    ret_list.append(view_desc_text)
+
+    ret = ','.join(ret_list)
+    ret = replace_comma_space(ret)
+
+    print(ret)
+    return ret
 
 
-def get_movement_list():
-    return []
+def get_x_list(x):
+    x = drop_down_config[x]
+    return list(map(lambda item: item["name"], x))
 
 
-def get_scene_list():
-    return []
+def get_x_list_item(x, select_name):
+    x_list = drop_down_config[x]
+    if select_name == 0:
+        return x_list[0]
+    select_list = list(filter(lambda item: item["name"] == select_name, x_list))
+    return select_list[0] if select_list else None
 
-
-def get_expression_list():
-    return []
 
 
 def ui():
-    hua_feng_list = get_hua_feng_list()
-    out_mode_list = get_out_mode_list()
-    people_list = get_people_list()
-    movement_list = get_movement_list()
-    scene_list = get_scene_list()
-    expression_list = get_expression_list()
+    common_list = get_x_list("commons")
+    out_mode_list = get_x_list("modes")
+    people_list = get_x_list("masters")
+    movement_list = get_x_list("poses")
+    scene_list = get_x_list("scenes")
+    expression_list = get_x_list("expressions")
+    other_desc_list = get_x_list("others")
+    r_desc_list = get_x_list("R_DESC")
+    clothes_list = get_x_list("clothes")
+    view_list = get_x_list("views")
 
     with gr.Column():
         gr.Markdown("# 这是一个预设组合提示词生成器")
-        gr.Markdown("## 质量+画风+风格+主体组合")
-    with gr.Column():
-        quality_text = gr.Textbox(label="输入质量描述,建议不必修改",
-                                  value="(best quality,4k,8k,highres,masterpiece:1.2),ultra-detailed,(realistic,photorealistic,photo-realistic:1.37)",
-                                  lines=2, elem_id="quality_text")
 
     with gr.Column():
         with gr.Row():
-            hua_feng = gr.Dropdown(hua_feng_list, label="选择作者画风（预设）", value=0, type="value", elem_id="hua_feng",
-                                   allow_custom_value=True)
+            common_text_list = gr.CheckboxGroup(choices=common_list, label="基础参数决策", elem_id="common_text_list")
             out_mode = gr.Dropdown(out_mode_list, label="选择输出风格", value=0, type="value", elem_id="out_mode",
                                    allow_custom_value=True)
 
@@ -150,6 +165,11 @@ def ui():
             # 人物选择 服饰五官等人物描述 枚举一些常用的即可
             people_desc = gr.Dropdown(people_list, label="选择人物角色", value=0, type="value", elem_id="people_desc",
                                       allow_custom_value=True)
+            # 服饰选择
+
+            clothes_desc = gr.Dropdown(clothes_list, label="选择服饰", value=0, type="value", elem_id="clothes_desc",
+                                      allow_custom_value=True)
+
             # 动作姿态选择 手势+姿态+互动
             movement_desc = gr.Dropdown(movement_list, label="选择人物动作", value=0, type="value",
                                         elem_id="movement_desc", allow_custom_value=True)
@@ -160,6 +180,16 @@ def ui():
             expression_desc = gr.Dropdown(expression_list, label="选择表情情感", value=0, type="value",
                                           elem_id="movement_desc", allow_custom_value=True)
 
+            # 视角
+            view_desc = gr.Dropdown(view_list, label="选择角度", value=0, type="value",
+                                          elem_id="view_desc", allow_custom_value=True)
+
+    with gr.Column():
+        with gr.Row():
+            other_desc_list_select = gr.CheckboxGroup(choices=other_desc_list, label="其他常用参数",
+                                                      elem_id="other_desc_list_select")
+            r_desc_list_select = gr.CheckboxGroup(choices=r_desc_list, label="R级参数",
+                                                  elem_id="r_desc_list")
         master_desc = gr.Textbox(label="输入主体其他自定义描述，比如 其他角色 或者 其他互动", lines=2)
 
     with gr.Column():
@@ -167,13 +197,16 @@ def ui():
         output_text = gr.Textbox(label="生成的提示词", lines=5)
 
         generater_btn.click(generater_tags,
-                            inputs=[quality_text, hua_feng, out_mode, people_desc, movement_desc, scene_desc,
-                                    expression_desc, master_desc], outputs=output_text)
+                            inputs=[common_text_list, out_mode, people_desc, movement_desc, scene_desc,
+                                    expression_desc, other_desc_list_select, r_desc_list_select,
+                                    master_desc, clothes_desc, view_desc],
+                            outputs=output_text)
 
-    return [quality_text, hua_feng, out_mode, master_desc, generater_btn, output_text]
+    return [common_text_list, out_mode, master_desc, generater_btn, output_text]
 
 
 if __name__ == '__main__':
+    load_config()
     with gr.Blocks() as demo:
         ui()
     demo.launch()
